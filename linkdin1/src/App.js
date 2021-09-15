2e783b5d8e68e0fe62d70ce2b4c21a6d4eef5e4d
dkumar24 <59641093+dkumar24@users.noreply.github.com>
2021-09-15 09:36:25 +0530

    Use state is done

--- linkdin1/src/App.js
+++ linkdin1/src/App.js
@@ -11,21 +11,31 @@ function Main(props)
 {
   return <section>
     {props.content}
+    <ul>
+      {
+        dishObjects.map((dish)=> <li key={dish.id}>{dish.title}</li>)
+      }
+    </ul>
   </section>
 };
 function Footer(props)
 {
   return <footer>Copy Right {props.year}</footer>
 };
+const dishes = ["Cheese",
+"Vegetables","Tufu"]
+
+const dishObjects = dishes.map((dish,i)=>({id:i,title:dish}))
 function App() {
   return (
     <div>
   <Header name="Deepak"/>
-    <Main content="You can Change Any Content Here"/>
+    <Main content="You can Change Any Content Here" dishes={dishes}/>
     <Footer year={new Date().getFullYear()}/>
     </div>
   
   );
+
 }
 
 export default App;
